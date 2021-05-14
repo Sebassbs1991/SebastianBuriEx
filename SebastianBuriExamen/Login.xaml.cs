f3a7751cafec61cf7cf6d1a022244b67c0885ad6
Sebastián Buri <sebastianburi@hotmail.com>
2021-05-14 00:57:48 -0500

    Carga

---
+++ SebastianBuriExamen/Login.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SebastianBuriExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUser.Text))
+            {
+                await DisplayAlert("Error", "Ingrese usuario", "Aceptar");
+                txtUser.Focus();
+                return;
+
+            }
+
+            if (string.IsNullOrEmpty(txtPass.Text))
+            {
+                await DisplayAlert("Error", "Ingrese contraseña", "Aceptar");
+                txtPass.Focus();
+                return;
+            }
+
+            if (txtUser.Text == "estudiante2021" && txtPass.Text == "uisrael2021")
+            {
+                string usuario = txtUser.Text;
+                string clave = txtPass.Text;
+
+                await Navigation.PushAsync(new Registro(usuario, clave));
+
+            }
+            else
+            {
+                await DisplayAlert("Error", "Usuario o clave incorrecta", "Aceptar");
+            }
+
+
+
+            
+        }
+    }
+}
---
+++ SebastianBuriExamen/Registro.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SebastianBuriExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario, string pass)
+        {
+            InitializeComponent();
+
+            lblUser.Text = usuario;
+            
+
+        }
+
+        private  void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            int costo=1800;
+            /*string nombre = txtNombre.Text;           
+            int monto = txtMonto.Text;
+            int pago = txtPago.Text;
+            
+
+            await Navigation.PushAsync(new Resumen(nombre, monto, pago));*/
+
+
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
